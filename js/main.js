51c8792394c8a22b6146728904fd2ebac7164873
d1xon41 <imdix0nlim@gmail.com>
2026-05-22 09:39:38 +1000

    added charts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 vegaEmbed('#map-parks', 'vega/map_parks.json', { actions: false });
 vegaEmbed('#map-choropleth', 'vega/map_choropleth.json', { actions: false });
-vegaEmbed('#bar-parks-count', 'vega/bar_parks_count.json', { actions: false });
+vegaEmbed('#bar-parks-count', 'vega/map_parks_count.json', { actions: false });
+vegaEmbed('#bar-percent-protected', 'vega/bar_percent_protected.json', { actions: false });
+vegaEmbed('#bar-iucn', 'vega/bar_iucn.json', { actions: false });
+vegaEmbed('#scatter-area', 'vega/scatter_area.json', { actions: false });
