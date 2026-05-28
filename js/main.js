025ce9d7c70dd55f4ec1637caf2d601522bd10d0
d1xon41 <imdix0nlim@gmail.com>
2026-05-29 07:27:44 +1000

    added time slider

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ vegaEmbed('#map-world',                   'vega/map_world.json',                
 vegaEmbed('#map-choropleth',              'vega/map_choropleth.json',               opt);
 vegaEmbed('#map-bubbles-interactive',     'vega/map_bubbles_interactive.json',      opt);
 vegaEmbed('#map-parks-count-choropleth',  'vega/map_parks_count_choropleth.json',   opt);
+vegaEmbed('#map-choropleth-time', 'vega/map_choropleth_time.json', opt);
 vegaEmbed('#bar-parks-count',             'vega/map_parks_count.json',              opt);
 vegaEmbed('#bar-percent-protected',       'vega/bar_percent_protected.json',        opt);
 vegaEmbed('#bar-iucn',                    'vega/bar_iucn.json',                     opt);
