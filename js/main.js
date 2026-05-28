353f20ac04624ed33fb9e3cf9269adeeb4c602cb
d1xon41 <imdix0nlim@gmail.com>
2026-05-29 08:43:09 +1000

    fixed errors

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ const opt = { actions: false };
 
 vegaEmbed('#map-world',                   'vega/map_world.json',                    opt);
 vegaEmbed('#map-choropleth',              'vega/map_choropleth.json',               opt);
-vegaEmbed('#map-choropleth-time', 'vega/map_choropleth_time.json', opt);
+vegaEmbed('#bar-percent-protected',       'vega/bar_percent_protected.json',        opt);
 vegaEmbed('#map-bubbles-interactive',     'vega/map_bubbles_interactive.json',      opt);
 vegaEmbed('#map-parks-count-choropleth',  'vega/map_parks_count_choropleth.json',   opt);
-vegaEmbed('#map-choropleth-time', 'vega/map_choropleth_time.json', opt);
+vegaEmbed('#map-choropleth-time',         'vega/map_choropleth_time.json',          opt);
 vegaEmbed('#bar-parks-count',             'vega/map_parks_count.json',              opt);
-vegaEmbed('#bar-percent-protected',       'vega/bar_percent_protected.json',        opt);
 vegaEmbed('#bar-iucn',                    'vega/bar_iucn.json',                     opt);
 vegaEmbed('#scatter-area',                'vega/scatter_area.json',                 opt);
 vegaEmbed('#bar-bioregions',              'vega/bar_bioregions.json',               opt);
